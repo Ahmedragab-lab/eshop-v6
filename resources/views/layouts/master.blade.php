5ed32dff0acbed4ee42f64ca7adbaf3b946ac4f5
wolf <ahmedragabyasin2020@gmail.com>
2022-01-15 15:01:23 +0200

    ecom theme

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layouts.frontcss')
+<body class="home-page home-01 ">
+	@include('layouts.minhead')
+	<!--header nav-->
+	@include('layouts.header')
+
+	@yield('content')
+    {{-- {{ $slot }} --}}
+    {{-- footer  --}}
+    @include('layouts.footer')
+	@include('layouts.frontjs')
+</body>
+</html>
