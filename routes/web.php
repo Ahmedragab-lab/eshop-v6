0f3d26162588ce235294fc34b1e6866661b4ef89
wolf <ahmedragabyasin2020@gmail.com>
2022-01-15 17:57:06 +0200

    livewire shop cart checkout

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Home;
+use App\Http\Livewire\Shop;
+use App\Http\Livewire\Cart;
+use App\Http\Livewire\Checkout;
 
-// Route::get('/', function () {
-//     return view('front');
-// });
+//start routes
 Route::get('/',Home::class);
+Route::get('/shop',Shop::class);
+Route::get('/cart',Cart::class);
+Route::get('/checkout',Checkout::class);
+
+
+
+
+
+
+
 // end of routes
